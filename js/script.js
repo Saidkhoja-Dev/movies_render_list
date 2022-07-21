2e1e1613d35906afc3b97f426b93e71c532df941
Saidaminxo'ja <saidaminxojam@gmail.com>
2022-07-21 09:50:12 +0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,153 @@
+var movies = [
+    {
+        "Title": "Room",
+        "fulltitle": "Room (2015)",
+        "movie_year": 2015,
+        "Categories": "Drama",
+        "summary": "ROOM tells the extraordinary story of Jack, a spirited 5-year-old who is looked after by his loving and devoted mother. Like any good mother, Ma dedicates herself to keeping Jack happy and safe, nurturing him with warmth and love and doing typical things like playing games and telling stories. Their life, however, is anything but typical--they are trapped--confined to a 10-by-10-foot space that Ma has euphemistically named Room. Ma has created a whole universe for Jack within Room, and she will stop at nothing to ensure that, even in this treacherous environment, Jack is able to live a complete and fulfilling life. But as Jack's curiosity about their situation grows, and Ma's resilience reaches its breaking point, they enact a risky plan to escape, ultimately bringing them face-to-face with what may turn out to be the scariest thing yet: the real world.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Room_%282015_film%29.png/220px-Room_%282015_film%29.png",
+        "imdb_id": "tt3170832",
+        "imdb_rating": 8.2,
+        "runtime": 118,
+        "language": "English",
+        "ytid": "-5UvJw7d1Vk"
+    },
+    {
+        "Title": "Code 46",
+        "fulltitle": "Code 46 (2003)",
+        "movie_year": 2003,
+        "Categories": "Action|Drama|Romance|Sci-Fi|Thriller",
+        "summary": "Code 46 is a love story set in a Brave New World-type near-future where cities are heavily controlled and only accessible through checkpoints. People cannot travel unless they have \"papeles\" (papers in Spanish; words and sentences in many languages, especially Spanish, French and Chinese are mixed with English in this new world), a special travel permit issued by the totalitarian government, the \"Sphinx\". Outside these cities, the desert has taken over and shanty towns are jammed with non-citizens - people without IDs forced to live primitive lives. William is a family man who works as a government investigator. When he is sent to Shanghai to solve a case of fake IDs, he meets a woman named Maria. Although he realizes she is behind the forgeries, he cannot help but fall completely in love with her. He hides her crime and they have a wild, passionate affair that can only last as long as his visa: 24 hours. Back home, William is obsessed with the memory of Maria. When the original ...",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/ru/thumb/a/a0/Poster_code_46.jpg/202px-Poster_code_46.jpg",
+        "imdb_id": "tt0345061",
+        "imdb_rating": 6.2,
+        "runtime": 93,
+        "language": "English",
+        "ytid": "-qP9dkJuRxk"
+    },
+    {
+        "Title": "Coconut Hero",
+        "fulltitle": "Coconut Hero (2015)",
+        "movie_year": 2015,
+        "Categories": "Action|Comedy|Drama",
+        "summary": "In the middle of Nowheresville lives 16-year-old Mike who wants out of his crappy life - permanently! Spending his time devising ways to do himself in, he considers the fatal brain tumor in his head as a godsend. That changes when he meets a girl and falls in love for the first time - without even knowing what love is.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Coconut_Hero_Poster.jpg/220px-Coconut_Hero_Poster.jpg",
+        "imdb_id": "tt4246854",
+        "imdb_rating": 6.3,
+        "runtime": 97,
+        "language": "English",
+        "ytid": "nt95FCJHRRw"
+    },
+    {
+        "Title": "Lego DC Comics Super Heroes: Justice League - Cosmic Clash",
+        "fulltitle": "Lego DC Comics Super Heroes: Justice League - Cosmic Clash (2016)",
+        "movie_year": 2016,
+        "Categories": "Action|Adventure|Animation|Comedy|Family",
+        "summary": "Can the Justice League prevail against an unstoppable intergalactic machine with the power to bend time itself!? Until now, the Justice League has triumphed over every foe who has challenged them. This time, they face Brainiac, a super-intelligent invulnerable machine whose mad quest endangers all life in the galaxy. When Brainiac zaps Superman, Wonder Woman and Green Lantern to different places in time, the race is on for Batman and The Flash to re-assemble the Justice League!",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/b/b3/Justice-League-Cosmic-Clash-397x500.jpeg/220px-Justice-League-Cosmic-Clash-397x500.jpeg",
+        "imdb_id": "tt5251438",
+        "imdb_rating": 6.5,
+        "runtime": 78,
+        "language": "English",
+        "ytid": "c1pZ7iZxkJU"
+    },
+    {
+        "Title": "Metallica Through the Never",
+        "fulltitle": "Metallica Through the Never (2013)",
+        "movie_year": 2013,
+        "Categories": "Action|Music",
+        "summary": "Trip, a young roadie for Metallica, is sent on an urgent mission during the band's show. But what seems like a simple assignment turns into a surreal adventure.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/b/bd/Metallica_Through_the_Never_film.jpg",
+        "imdb_id": "tt2172935",
+        "imdb_rating": 7.2,
+        "runtime": 93,
+        "language": "English",
+        "ytid": "N4IhWJ8r7_4"
+    },
+    {
+        "Title": "Going Clear: Scientology & the Prison of Belief",
+        "fulltitle": "Going Clear: Scientology & the Prison of Belief (2015)",
+        "movie_year": 2015,
+        "Categories": "Action|Documentary",
+        "summary": "A devastating two hour documentary based on Lawrence Wright's book of the same name. Scientology is laid bare by a film that skilfully knits together archive footage, testimonials from former high ranking officials and public, and dramatic reconstructions.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/4/4d/Going_Clear_Poster.jpg",
+        "imdb_id": "tt4257858",
+        "imdb_rating": 8,
+        "runtime": 119,
+        "language": "English",
+        "ytid": "ixgd38EZIR0"
+    },
+    {
+        "Title": "Slither",
+        "fulltitle": "Slither (2006)",
+        "movie_year": 2006,
+        "Categories": "Action|Comedy|Horror|Sci-Fi",
+        "summary": "In this blend of the B movie classic The Blob (1958), and some Romero's zombies film, a meteorite collides in a small town. Grant finds it, and is infected by a parasite worm, which installs in his brain and causes him a creepy transformation into a monster. Starla, his wife, and Bill, a policeman, will try to stop him and the plague of worms generated by the creature.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Slithermovieposter.jpg/220px-Slithermovieposter.jpg",
+        "imdb_id": "tt0439815",
+        "imdb_rating": 6.5,
+        "runtime": 95,
+        "language": "English",
+        "ytid": "2-f8wU6Fpeo"
+    },
+    {
+        "Title": "Creed",
+        "fulltitle": "Creed (2015)",
+        "movie_year": 2015,
+        "Categories": "Action|Drama|Sport",
+        "summary": "Adonis Johnson is the son of the famous boxing champion Apollo Creed, who died in a boxing match in Rocky IV (1985). Adonis wasn't born until after his father's death and wants to follow his fathers footsteps in boxing. He seeks a mentor who is the former heavyweight boxing champion and former friend of Apollo Creed, the retired Rocky Balboa. Rocky eventually agrees to mentor Adonis. With Rocky's help they hope to get a title job to face even deadlier opponents than his father. But whether he is a true fighter remains to be seen....",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/2/24/Creed_poster.jpg/220px-Creed_poster.jpg",
+        "imdb_id": "tt3076658",
+        "imdb_rating": 7.6,
+        "runtime": 133,
+        "language": "English",
+        "ytid": "fCBzWLVQgk8"
+    },
+    {
+        "Title": "Moonwalkers",
+        "fulltitle": "Moonwalkers (2015)",
+        "movie_year": 2015,
+        "Categories": "Action|Comedy",
+        "summary": "What if Apollo 11 never actually made it? What if, in reality, Stanley Kubrick secretly shot the famous images of the moon landing in a studio, working for the US administration? This is the premise of a totally plausible conspiracy theory that takes us to swinging sixties London, where a stubborn CIA agent will never find Kubrick but instead is forced to team up with a lousy manager of a seedy rock band to develop the biggest con of all time, in this riotous, high-tempo action-comedy.",
+        "ImageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Moonwalkers_Movie_Poster.jpg/220px-Moonwalkers_Movie_Poster.jpg",
+        "imdb_id": "tt2718440",
+        "imdb_rating": 6.1,
+        "runtime": 107,
+        "language": "English",
+        "ytid": "5J4vTI0Qr5k"
+    }
+];
+
+var elList = document.querySelector(".list");
+
+function renderMovie(p) {
+    
+    for (let i = 0; i < movies.length; i++) {
+        var listOfMovies = document.createElement("li");
+        elList.appendChild(listOfMovies);
+        
+        for (let j = 0; j < 1; j++) {
+            var headingTwo = document.createElement("h2");
+            listOfMovies.appendChild(headingTwo);
+            headingTwo.textContent = movies[i]["Title"];
+
+            for (let j = 0; j < 1; j++) {
+                var img = document.createElement("img");
+                listOfMovies.appendChild(img);
+                img.src = movies[i]["ImageURL"];
+                img.width = 300
+                img.height = 350;
+                var elDivBox =document.createElement("div");
+                var elParagraph =document.createElement("p");
+                listOfMovies.appendChild(elDivBox);
+                elDivBox.appendChild(elParagraph);
+                elDivBox.textContent = movies[i]["summary"];
+                elDivBox.width = 300
+                elDivBox.height = 300
+                elDivBox.classList.add("box");
+            } 
+        }
+    } 
+}
+
+renderMovie()
